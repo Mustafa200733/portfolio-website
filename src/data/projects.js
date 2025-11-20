a3f30a8fec6730e5f83688c80c611aaeceefbda2
9024454 <9024454@student.zadkine.nl>
2025-11-20 11:05:37 +0100

    Updates

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+const projects = [
+  {
+  id: 1,  
+  name: "Escape room",
+  description: "Dit is de homepage van mijn Escape room die ik heb gecodeerd in de 1ste jaar.",
+  image:"/public/escaperoom.png",
+
+  },
+
+    {
+  id: 2,
+  name: "Bread Company",
+  description: "Dit is de homepage van mijn Bread Company die ik heb gecodeerd ",
+  image:"/public/Breadcompany.png",
+
+  },
+
+    {
+  id: 3,
+  name: "Fitness App",
+  description: "Dit is een fitnness app die ik vorig jaar heb gedisigned",
+  image:"/public/fitness.png",
+  },
+  ];
+  
+export default projects;
